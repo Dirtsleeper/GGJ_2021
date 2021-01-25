c65d9add509069a4d6b2e58ffb6be2e960ad4d1d
Casey Seitz <seitzcasey@gmail.com>
2021-01-25 09:10:15 -0500

    Fixed folder structure for .gitignore.

--- GGJ_2021/Source/GGJ_2021/GGJ_2021.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GGJ_2021.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GGJ_2021, "GGJ_2021" );
--- GGJ_2021/Source/GGJ_2021/GGJ_2021GameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "GGJ_2021GameModeBase.h"
-

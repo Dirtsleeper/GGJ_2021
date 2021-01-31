2cfe1f99119364c9e04f1953c5266bcf3e64278a
Dirtsleeper <DirtsleeperGames@gmail.com>
2021-01-31 11:15:36 -0500

    Added font as default.

--- Source/GGJ_2021/GGJ_TextBlock.cpp
+++ Source/GGJ_2021/GGJ_TextBlock.cpp
@@ -2,9 +2,11 @@
 
 
 #include "GGJ_TextBlock.h"
+#include "Engine/Font.h"
 
 UGGJ_TextBlock::UGGJ_TextBlock(const FObjectInitializer& initializer)
 	: Super(initializer)
 {
-	
+	static ConstructorHelpers::FObjectFinder<UFont> MyFontObj(TEXT("/Game/Assets/MyFonts/MilkieWylkie_Font.MilkieWylkie_Font"));
+	Font = FSlateFontInfo(MyFontObj.Object, 24, FName("Regular"));
 }
